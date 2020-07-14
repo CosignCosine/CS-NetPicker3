3560904c9abb2a13a887e273856952f80b6478bc
Scott Schraeder <CosignCosine@users.noreply.github.com>
2020-07-14 06:54:46 -0400

    Fin

---
+++ CS-NetPicker3/Ingame.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICities;
+using UnityEngine;
+
+namespace CS_NetPicker3
+{
+    public class IngameLoader : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            base.OnLevelLoaded(mode);
+
+            if (NetPickerTool.instance == null)
+            {
+                ToolController toolController = ToolsModifierControl.toolController;
+                NetPickerTool.instance = toolController.gameObject.AddComponent<NetPickerTool>();
+                NetPickerTool.instance.enabled = false;
+            }
+        }
+
+        public override void OnReleased()
+        {
+            base.OnReleased();
+            GameObject.Destroy(NetPickerTool.instance);
+        }
+
+        public override void OnLevelUnloading()
+        {
+            base.OnLevelUnloading();
+            GameObject.Destroy(NetPickerTool.instance);
+        }
+
+        public override void OnCreated(ILoading loading)
+        {
+            base.OnCreated(loading);
+            if (LoadingManager.instance.m_loadingComplete)
+            {
+                if (NetPickerTool.instance == null)
+                {
+                    ToolController toolController = ToolsModifierControl.toolController;
+                    NetPickerTool.instance = toolController.gameObject.AddComponent<NetPickerTool>();
+                    NetPickerTool.instance.enabled = false;
+                }
+            }
+        }
+    }
+
+    public class IngameKeybindingResolver : ThreadingExtensionBase
+    {
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                Debug.Log("N key");
+                NetPickerTool.instance.enabled = !NetPickerTool.instance.enabled;
+                ToolsModifierControl.SetTool<NetPickerTool>();
+            }
+        }
+    }
+}
---
+++ CS-NetPicker3/Mod.cs
@@ -0,0 +1,29 @@
+﻿using ICities;
+
+namespace CS_NetPicker3
+{
+    public class NetPicker : IUserMod
+    {
+        public string Name => "Prefab Picker";
+        public string Description => "Eyedrop any object from the map.";
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            UIHelperBase keybindingsGroup = helper.AddGroup("Keybindings");
+            keybindingsGroup.AddCheckbox("Ctrl", false, (check) =>
+            {
+
+            });
+
+            keybindingsGroup.AddCheckbox("Alt", false, (check) =>
+            {
+
+            });
+
+            keybindingsGroup.AddCheckbox("Shift", false, (check) =>
+            {
+
+            });
+        }
+    }
+}
